581152f8d5c0630889d1a09fe27d3d1c4c14016a
Zytronium <dastelljes@outlook.com>
2025-03-02 15:51:29 -0600

    Possibly finished task 5 correctly

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,33 @@
+export default class Building {
+  constructor(sqft) {
+    // Type validation
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    // Private attribute
+    this._sqft = sqft;
+    // Enforce all subclasses have implemented evacuationWarningMessage()
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error("Class extending Building must override evacuationWarningMessage")
+    }
+
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Setter for sqft
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error("evacuationWarningMessage is not yet implemented");
+  }
+
+}
