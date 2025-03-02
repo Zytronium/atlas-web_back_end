b26b5d3cb9ec65e41fb8e0c5d7fa59cbbc3c1c51
Zytronium <dastelljes@outlook.com>
2025-03-01 18:25:45 -0600

    Attempted to fix task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
+  /* eslint-disable no-unused-vars */
   var task = false;
+  /* eslint-disable no-unused-vars */
   var task2 = true;
 
   /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
     var task3 = true;
+    /* eslint-disable no-unused-vars */
     var task4 = false;
   }
   /* eslint-disable no-unused-vars */
