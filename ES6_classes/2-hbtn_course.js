8ec20063a59ffef38b4b9b268782f6bda4c2ac41
Zytronium <dastelljes@outlook.com>
2025-03-02 14:49:49 -0600

    Finished task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,19 +1,58 @@
 export default class HolbertonCourse {
+  // constructors
   constructor(name, length, students) {
+    // type validation
     if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
+      throw new TypeError('Name must be a string');
     }
 
     if (typeof length !== 'number') {
-      throw new TypeError('length must be a number');
+      throw new TypeError('Length must be a number');
     }
 
     if (!(Array.isArray(students) && students.every(student => typeof student === 'string'))) {
-      throw new TypeError('students must be an array of strings');
+      throw new TypeError('Students must be an array of strings');
     }
 
+    // private attributes
     this._name = name;
     this._length = length;
     this._students = students;
   }
+
+  // getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // setters
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  set students(students) {
+    if (!(Array.isArray(students) && students.every(student => typeof student === 'string'))) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = students;
+  }
+
 }
