f4791bf55a4cdf8cd0c7a829e8ca50be17083c9a
Zytronium <dastelljes@outlook.com>
2025-03-01 19:51:36 -0600

    Actually fixed that linter check that turned out to not be a false positive (I just misinterpreted it)

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,12 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let newArray = array;
+  const newArray = array;
   for (const value of array) {
     const idx = array.indexOf(value);
     newArray[idx] = appendString + value;
   }
-
-  // Fix false positive in linter check that says newArray is never modified
-  newArray[newArray.length - 1] = newArray[newArray.length - 1];
-
   return newArray;
 }
