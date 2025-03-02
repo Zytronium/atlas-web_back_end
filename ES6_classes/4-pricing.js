da9a6dd932e93f953825775bd9356669fcc046de
Zytronium <dastelljes@outlook.com>
2025-03-02 15:18:31 -0600

    Started task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    // type validation
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
+    }
+
+    // private attributes
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // getters and setters
+
+  // Amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  // Currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
+    }
+    this._currency = currency;
+  }
+}
