a30d948a35863b82679e6086275dffa80b710972
Zytronium <dastelljes@outlook.com>
2025-03-07 19:06:29 -0600

    Fixed task 3 formatting

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(students) {
-  return students.reduce((sum, student) => { return student.id + sum; }, 0);
+  return students.reduce((sum, student) => student.id + sum, 0);
 }
