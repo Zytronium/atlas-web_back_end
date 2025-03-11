f4a42dc1a7154fe77020656a3d78a9727de305ad
Zytronium <dastelljes@outlook.com>
2025-03-11 11:58:01 -0500

    Fixed task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,17 @@
 export default function cleanSet(set, startString) {
   let cleanString = '';
 
-  if (startString === '' || startString === undefined) {
+  // Return empty string if startString is empty or set is no all strings
+  if (startString === '') {
     return '';
   }
+  for (const item of set) {
+    if (typeof item !== 'string') {
+      return '';
+    }
+  }
 
+  // Find all strings in the set that start with the startString
   set.forEach((item) => {
     if (item.startsWith(startString)) {
       cleanString += `-${item.replace(startString, '')}`;
