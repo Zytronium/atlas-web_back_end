70a6b76d3add9c5baf54fa22ab739dcc2e45bdf5
Zytronium <dastelljes@outlook.com>
2025-03-10 19:07:03 -0500

    Started and skipped task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,15 @@
+export default function createInt8TypedArray(length, position, value) {
+  return ArrayBuffer(length); // I don't understand this one at all. Skipping it for now
+
+  // skipity toy let
+}
+
+/*
+*
+* returns: new ArrayBuffer with an Int8 value at a specific position
+*
+* params:
+* length - number
+* position - number
+* value - number
+*/
