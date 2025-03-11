3582cede2dfdbe69d3a73c7f8ff2788395d86a16
Zytronium <dastelljes@outlook.com>
2025-03-11 12:28:38 -0500

    Small formatting adjustment (added a newline)

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,6 +2,7 @@ export default function updateUniqueItems(groceryListMap) {
   if (!(groceryListMap instanceof Map)) {
     throw new Error('Cannot process');
   }
+
   groceryListMap.forEach((value, key) => {
     if (value === 1) {
       groceryListMap.set(key, 100);
