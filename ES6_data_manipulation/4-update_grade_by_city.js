7f3015975ed11a49eecce3399d0a017bc7ecd15f
Zytronium <dastelljes@outlook.com>
2025-03-10 19:00:28 -0500

    Rewrote task 4 to comply with the linter

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const newList = students.filter((student) => student.location === city);
-  const updateIds = newGrades.map((grade) => grade.studentId);
-
-  newList.forEach((student) => {
-    if (updateIds.includes(student.id)) {
-      student.grade = newGrades.find((grade) => grade.studentId === student.id).grade;
-    } else {
-      student.grade = 'N/A';
-    }
+  return students.filter((student) => student.location === city).map((student) => {
+    const grade = newGrades.find((grd) => grd.studentId === student.id);
+    return {
+      ...student, grade: grade ? grade.grade : 'N/A',
+    };
   });
-
-  return newList;
 }
 
 /*
