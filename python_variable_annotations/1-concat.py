24d90df73bd5a30e6326f28fc1ef0e8eaadb99b5
zytronium <dastelljes@outlook.com>
2025-05-08 16:53:01 -0500

    Added a second newline to end of file to appease the checker

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -6,9 +6,10 @@ Concat documentation is below
 
 def concat(str1: str, str2: str) -> str:
     """
-    Concatinates two strings.
+    Concatenates two strings.
     :param str1: string one
     :param str2: string two
     :return: str1 plus str2
     """
     return str1 + str2
+
