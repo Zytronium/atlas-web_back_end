d6ee2fc730b04bb1b3144abfe7afe3b9c3230eb7
zytronium <dastelljes@outlook.com>
2025-05-08 17:08:02 -0500

    Did task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function for task 3
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns n as a string
+    :param n: float
+    :return: n as a string
+    """
+    return str(n)
