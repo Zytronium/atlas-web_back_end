038b9ba4a16466941e12bd1f9b437ee30dfcd437
zytronium <dastelljes@outlook.com>
2025-05-08 17:48:20 -0500

    Did task 9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function for task 9
+"""
+import typing
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """
+    I don't want to make any more documentation
+    :param lst: 1st element
+    :return: length of the 1st element
+    """
+    return [(i, len(i)) for i in lst]
