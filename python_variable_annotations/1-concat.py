b7221ef44e3cb1065d07f4ba5b1c934faeebd01d
zytronium <dastelljes@outlook.com>
2025-05-08 16:51:53 -0500

    Did task 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""
+Concatinates two strings.
+:param str1: string one
+:param str2: string two
+:return: str1 plus str2
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
