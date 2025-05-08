a2deae5e16acf0fbb9c035b3b29dd3028ac3f3dc
zytronium <dastelljes@outlook.com>
2025-05-08 17:48:53 -0500

    Fixed task 9 formatting

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,7 +5,8 @@ function for task 9
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence])\
+        -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """
     I don't want to make any more documentation
     :param lst: 1st element
