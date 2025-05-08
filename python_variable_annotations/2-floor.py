5f19be7e41fe8ae93a4f96c02a57a6f5569b9a83
zytronium <dastelljes@outlook.com>
2025-05-08 17:06:26 -0500

    Did task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+function for task 2
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of n as an integer.
+    :param n: float to floor
+    :return: the floor of n
+    """
+    return math.floor(n)
