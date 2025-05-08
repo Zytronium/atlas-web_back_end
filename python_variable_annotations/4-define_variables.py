71868e3219a202ef55c55649d78ce14ff117a882
zytronium <dastelljes@outlook.com>
2025-05-08 17:18:45 -0500

    Did task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+function for task 3
+"""
+
+
+a: int = 1  # How offensively simple. Just 1? That's not inclusive at all! How about several billion?
+pi: float = 3.14  # How offensively imprecise. We all know Pi = 3.14159265358979323846264338327952846246... I think I got that right, though I might've botched the last 8 digits
+i_understand_annotations: bool = True  # Actually this one's quite valid.
+school: str = "Holberton"  # Blasphemy
