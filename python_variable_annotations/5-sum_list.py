59182ffd04cb6d37da65869170ebccdceb3c3236
zytronium <dastelljes@outlook.com>
2025-05-08 17:30:52 -0500

    Did task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function for task 5
+"""
+
+
+def sum_list(input_list: list) -> float:
+    """
+    Takes a list of floats as argument and returns their sum as a float.
+    :param input_list: list of floats
+    :return: the sum of the given input list
+    """
+    return sum(input_list)
