60fead9a90cd2538872e3fd7d52eb99091f547c5
zytronium <dastelljes@outlook.com>
2025-05-08 17:27:09 -0500

    Restyled comments to appease the checker

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -4,7 +4,14 @@ Variables required for task 3
 """
 
 
-a: int = 1  # How offensively simple. Just 1? That's not inclusive at all! How about several billion?
-pi: float = 3.14  # How offensively imprecise. We all know Pi = 3.14159265358979323846264338327952846246... I think I got that right, though I might've botched the last 8 digits.
+
+a: int = 1  # How offensively simple. Just 1?
+            # That's not inclusive at all! How about several billion?
+
+pi: float = 3.14  # How offensively imprecise. We all
+                  # know Pi = 3.14159265358979323846264338327950284626…
+                  # I think that's right… Might've botched the last few digits
+
 i_understand_annotations: bool = True  # Actually this one's quite valid.
+
 school: str = "Holberton"  # Blasphemy.
