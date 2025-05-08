466c79bdd3399658a51818d378defbb0148f3880
zytronium <dastelljes@outlook.com>
2025-05-08 16:46:26 -0500

    Added documentation to task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
+    """
+    adds a and b and returns the sum.
+    :param a: first number
+    :param b: number to add to a
+    :return: sum of a and b
+    """
     return a + b
