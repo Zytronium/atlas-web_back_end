c4b69922562eac18e9f9b636c89f90a77eac7104
zytronium <dastelljes@outlook.com>
2025-05-09 16:22:01 -0500

    Fixed a typo

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Takes a max delay and returns an wait_random(max_delay) as an asyncio.Task
+    Takes a max delay and returns wait_random(max_delay) as an asyncio.Task
     :param max_delay: the max delay to feed to wait_random
     :return: wait_random(max_delay) as an asyncio.Task
     """
