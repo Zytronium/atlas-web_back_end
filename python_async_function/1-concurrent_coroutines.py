0129e9c0cab06f3e55627b029c01927ccaeff614
zytronium <dastelljes@outlook.com>
2025-05-09 15:26:30 -0500

    Did task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+file for task 1
+"""
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """
+    Calls wait_random() n times using a max delay of max_delay
+    :param n: Number of times to run wait_random()
+    :param max_delay: Max delay per calling wait_random()
+    :return: A list of all the delays used.
+    """
+    delays: list[float] = []
+    for i in range(n):
+        # Divide by 5 to speed up testing process, multiply result to revert
+        delays.append(await wait_random(max_delay / 5) * 5)
+
+    return delays
