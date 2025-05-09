bc61ec3df388c641e4ba16cfa89d2cb1a3fbb35e
zytronium <dastelljes@outlook.com>
2025-05-09 15:35:48 -0500

    Sorted the delays list in ascending order

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -18,4 +18,4 @@ async def wait_n(n: int, max_delay: int) -> typing.List[float]:
         # Divide by 5 to speed up testing process, multiply result to revert
         delays.append(await wait_random(max_delay / 5) * 5)
 
-    return delays
+    return sorted(delays)
