1b8a05df6173051cf0503c12e402d21c000e64ce
Zytronium <dastelljes@outlook.com>
2025-05-26 14:38:13 -0500

    Finished task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+from re import sub
 
 
 def filter_datum(fields: list[str], redaction: str, message: str,
                  separator: str) -> str:
     """
-    Returns the log message obfuscated
+    Returns the log message with certain parts obfuscated.
     :param fields: List of strings representing all fields to obfuscate
     :param redaction: String representing by what the field will be obfuscated
     :param message: The log line
     :param separator: Which character is separating all fields in the log line
     :return: Obfuscated log message
     """
-
+    return sub(rf'({"|".join(fields)})=.*?{separator}',
+               lambda m: f"{m.group(1)}={redaction}{separator}",
+               message)
