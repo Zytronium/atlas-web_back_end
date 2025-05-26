2c75518f36018edf1fe8422cfb10911e964d968c
Zytronium <dastelljes@outlook.com>
2025-05-26 17:53:07 -0500

    Did task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -13,3 +13,13 @@ def hash_password(password: str) -> bytes:
     :return: a hashed, encrypted password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if the hashed password is matches the password
+    :param hashed_password: the hashed password
+    :param password: the password
+    :return: true if the hashed password is valid, false otherwise
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
