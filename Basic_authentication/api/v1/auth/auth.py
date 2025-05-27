649a7b5e653084e60224f6c16d4f2d893e790a9e
Zytronium <dastelljes@outlook.com>
2025-05-26 19:14:06 -0500

    Fixed task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -14,14 +14,23 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        I honestly don't know what this is for; I just followed instructions
-        to make it
+        Returns False if path is in excluded paths, else, True.
+        Ensures slash tolerant paths.
         :param path: path for something I guess
         :param excluded_paths: paths to exclude I guess
         :return: False if path is in excluded_paths, else, True
         """
-        if path is None or excluded_paths is None or len(excluded_paths) == 0 or path not in excluded_paths: return True
-        else: return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        normalized_path = path if path.endswith('/') else path + '/'
+
+        for excl_pth in excluded_paths:
+            excl_pth_norm = excl_pth if excl_pth.endswith(
+                '/') else excl_pth + '/'
+            if normalized_path == excl_pth_norm:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
