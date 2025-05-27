efbde8a1e1f62a5e2746900920b65cfe22f6facd
Zytronium <dastelljes@outlook.com>
2025-05-27 12:56:52 -0500

    Did task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """
 Basic Auth class
 """
+import base64
+
 from api.v1.auth.auth import Auth
 
 
@@ -11,3 +13,17 @@ class BasicAuth(Auth):
     """
     def __init__(self):
         pass
+
+    #  Or, if this were C, extrbauthead(), or even xb64ah()
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header for a Basic
+        Authentication request
+        :param authorization_header: The Authorization header
+        :return: The Base64 part of the Authorization header
+        """
+        return (None if (authorization_header is None
+                        or type(authorization_header) is not str
+                        or not authorization_header.startswith('Basic '))
+                else authorization_header.replace('Basic ', ''))
