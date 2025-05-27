65470143268317077a7d88babfa35554cb623742
Zytronium <dastelljes@outlook.com>
2025-05-27 17:39:55 -0500

    Fixed task 8 formatting

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -42,7 +42,8 @@ def session_login():
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def session_logout():
     """
     Session logout view
