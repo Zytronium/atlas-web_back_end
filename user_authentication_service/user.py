0a9cf44f37803138e6efa4a781d48f1b37a9198b
Zytronium <dastelljes@outlook.com>
2025-05-27 18:40:07 -0500

    Documented task user.py

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -6,9 +6,13 @@ User
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
 class User(Base):
+    """
+    User class
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
