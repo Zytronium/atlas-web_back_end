d91f3e131bb65c0d91b0591ee0add3abeb22e789
Zytronium <dastelljes@outlook.com>
2025-05-27 15:58:00 -0500

    Did task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,8 @@
 """
 Session Auth class
 """
+
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -9,4 +11,20 @@ class SessionAuth(Auth):
     """
     Session Auth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def __init__(self):
+        pass
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session ID for the given user_id
+        :param user_id: The User ID to create the session for
+        :return: The session ID string if successful, else None
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
