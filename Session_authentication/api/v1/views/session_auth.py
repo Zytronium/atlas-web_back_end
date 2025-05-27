4eaf553c8ab78005f0de72eb434f4fa3365ace26
Zytronium <dastelljes@outlook.com>
2025-05-27 17:35:21 -0500

    Did task 8

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -40,3 +40,15 @@ def session_login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def session_logout():
+    """
+    Session logout view
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abord(404)
+
+    return jsonify({}), 200
