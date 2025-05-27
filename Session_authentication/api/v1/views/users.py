4a42255b1251416a0da9c2ddb146fda998966008
Zytronium <dastelljes@outlook.com>
2025-05-27 15:14:22 -0500

    Did task 0

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -24,9 +24,13 @@ def view_one_user(user_id: str = None) -> str:
     Return:
       - User object JSON represented
       - 404 if the User ID doesn't exist
+      - Current user if <user_id> is 'me
     """
-    if user_id is None:
+    if user_id is None or (user_id == "me" and request.current_user is None):
         abort(404)
+    if user_id == "me":
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
