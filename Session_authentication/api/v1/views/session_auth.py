c4e1a3a621838ad28f599bddcce37aff6725e77b
Zytronium <dastelljes@outlook.com>
2025-05-27 17:24:53 -0500

    Fixed formatting

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -24,12 +24,12 @@ def session_login():
 
     users = User.search({'email': email})
     if not users or len(users) == 0:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
 
     user = users[0]
 
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
