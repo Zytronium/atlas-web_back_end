049c4f8811c5ed1cb787b7c71387eaa820c4e60f
Zytronium <dastelljes@outlook.com>
2025-05-26 20:09:15 -0500

    Fixed import

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
-from Basic_authentication.api.v1.auth.auth import Auth
+from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
