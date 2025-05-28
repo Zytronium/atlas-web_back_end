a58b1ba451a65facdfec59d60974b6fdc742a0dd
Zytronium <dastelljes@outlook.com>
2025-05-28 17:23:08 -0500

    Fixed task 8 annotations

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -26,7 +26,7 @@ class Auth:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
 
-    def valid_login(self, email, password) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """Checks if the user login is valid
         """
         user = self._db.find_user_by(email=email)
@@ -43,3 +43,9 @@ def _hash_password(password: str) -> bytes:
     Hashes a password using bcrypt and returns the hashed password in bytes
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def _generate_uuid():
+    """
+    Generates a random UUID
+    """
+    pass
