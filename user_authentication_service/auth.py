994ecb8ff10d2a84aa38d88279d32af06d51016d
Zytronium <dastelljes@outlook.com>
2025-05-28 17:21:35 -0500

    Fixed task 8 formatting

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -31,7 +31,8 @@ class Auth:
         """
         user = self._db.find_user_by(email=email)
 
-        if user and bcrypt.checkpw(password.encode("utf-8"), user.hashed_password):
+        if user and bcrypt.checkpw(password.encode("utf-8"),
+                                   user.hashed_password):
             return True
 
         return False
