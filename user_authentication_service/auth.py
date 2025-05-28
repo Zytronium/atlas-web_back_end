d29709091f1564767e7c74d268629af79d42ed37
Zytronium <dastelljes@outlook.com>
2025-05-27 19:54:19 -0500

    Did task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 import bcrypt
+from sqlalchemy.exc import NoResultFound
+
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user with the database with a hashed password.
+        """
+        try:  # If this raises a NoResultFound error, user doesn't exist yet
+            self._db.find_user_by(email=email)
+            #  If above line doesn't raise NoResultFound, user already exists
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            #  User doesn't already exist, procede with creating user
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
 
 
 def _hash_password(password: str) -> bytes:
