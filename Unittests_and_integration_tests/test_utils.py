0a150d0d8296b18ae83809d945cf796652b041a0
Zytronium <dastelljes@outlook.com>
2025-06-03 17:30:05 -0500

    Did task 3

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -7,10 +7,13 @@ import unittest
 from unittest.mock import Mock, patch
 
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test Case for access_nested_map() function
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -28,6 +31,11 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(str(error_context.exception), f"'{path[-1]}'")
 
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test Case for get_json() function
+    """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
@@ -42,3 +50,31 @@ class TestAccessNestedMap(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test Case for memoize() function
+    """
+    def test_memoize(self):
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with (patch.object(TestClass, "a_method", return_value=42)
+              as mock_a_method):
+            test_instance = TestClass()
+
+            # 1st call should call a_method
+            result1 = test_instance.a_property
+            # 2nd call should use cached return value from first call
+            result2 = test_instance.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, result1)
+            mock_a_method.assert_called_once()
