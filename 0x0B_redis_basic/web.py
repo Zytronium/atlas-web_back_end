0ed56aba600c9d22b691f472a02d84b9db66b1e7
Zytronium <dastelljes@outlook.com>
2025-06-11 16:29:14 -0500

    Did the first part of the advanced task 5.

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+File for advanced task 5. Implementing an expiring web cache and tracker
+Uses the requests module to obain HTML content of a page and returns it.
+"""
+
+import requests
+import redis
+
+
+db = redis.Redis()
+
+def get_page(url: str) -> str:
+    """
+    Gets the HTML content of a page from it's URL
+    :param url: URL of the page to pull HTML content from
+    :return: The HTML content of the given page
+    """
+    db.incr(f"{url}:count")
+
+    html = db.get(url)
+    if html is None:
+        html = requests.get(url).text
+        db.set(url, html, ex=10)
+    else:
+        html = html.decode("UTF-8")
+
+    return html
