da56ed914b063cc90446aaea49b29b0c30b59170
Zytronium <dastelljes@outlook.com>
2025-06-13 17:57:39 -0500

    Attempted to fix task 3 yet again

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 SELECT
     band_name,
     CASE
-        WHEN split IS NULL THEN YEAR(CURDATE()) - YEAR(formed)
+        WHEN split IS NULL OR split = 0 THEN YEAR(CURDATE()) - YEAR(formed)
         ELSE YEAR(split) - YEAR(formed)
     END AS lifespan
 FROM
