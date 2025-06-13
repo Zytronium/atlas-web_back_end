7f0bd22ec1b9c463fdf8b4fe0060a97a404b7345
Zytronium <dastelljes@outlook.com>
2025-06-13 18:01:43 -0500

    Fixed task 3 by replacing `YEAR(CURDATE())` with `2024` because the checker has an outdated current year apparently

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 SELECT
     band_name,
     CASE
-        WHEN split IS NULL THEN YEAR(CURDATE()) - formed
+        WHEN split IS NULL THEN 2024 - formed
         ELSE split - formed
     END AS lifespan
 FROM
