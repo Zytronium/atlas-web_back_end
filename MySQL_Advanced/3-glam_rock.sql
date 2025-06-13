bd6f66c3c62936e4a12868eec893508eddc0bdb0
Zytronium <dastelljes@outlook.com>
2025-06-13 17:46:53 -0500

    Attempted to fix task 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Lists all bands with Glam Rock as their main style, ranked by their longevity
 SELECT
     band_name,
-    IFNULL(YEAR(split), YEAR(CURDATE())) - year(FORMED) as lifespan
+    IFNULL(YEAR(split), 2025) - YEAR(formed) AS lifespan
 FROM
     metal_bands
 WHERE
