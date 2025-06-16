f90998c3446495321dfc729ba8ea858e1b0268ff
Zytronium <dastelljes@outlook.com>
2025-06-15 19:02:12 -0500

    Fixed issue with task 12 where it would run on import

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,18 +6,19 @@ Provides some stats about Nginx logs stored in MongoDB
 from pymongo import MongoClient
 
 
-client = MongoClient()
-collection = client["logs"]["nginx"]
+if __name__ == "__main__":
+    client = MongoClient()
+    collection = client["logs"]["nginx"]
 
-total_logs = collection.count_documents({})
-method_get = collection.count_documents({"method": "GET"})
-method_post = collection.count_documents({"method": "POST"})
-method_put = collection.count_documents({"method": "PUT"})
-method_patch = collection.count_documents({"method": "PATCH"})
-method_delete = collection.count_documents({"method": "DELETE"})
-status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    total_logs = collection.count_documents({})
+    method_get = collection.count_documents({"method": "GET"})
+    method_post = collection.count_documents({"method": "POST"})
+    method_put = collection.count_documents({"method": "PUT"})
+    method_patch = collection.count_documents({"method": "PATCH"})
+    method_delete = collection.count_documents({"method": "DELETE"})
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
 
-print(f"""{total_logs} logs
+    print(f"""{total_logs} logs
 Methods:
     method GET: {method_get}
     method POST: {method_post}
