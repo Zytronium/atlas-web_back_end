6375a790afec641f6b96a08a117526152c172473
Zytronium <dastelljes@outlook.com>
2025-06-23 19:43:52 -0500

    Did task 3

---
+++ Node_JS/3-read_file_async.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'UTF8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n');
+      const students = lines.slice(1).filter(line => line.trim() !== '');
+
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {};
+
+      for (const line of students) {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+
+      for (const [field, names] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+
+      resolve()
+    });
+  });
+}
+
+module.exports = countStudents;
