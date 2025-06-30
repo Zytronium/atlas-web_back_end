971334c2a85e8440e6b5af3a2ebbded23a7baa3a
Zytronium <dastelljes@outlook.com>
2025-06-30 18:27:14 -0500

    Did first half of task 6

---
+++ unittests_in_js/6-payment_token.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function getPaymentTokenFromAPI(success) {
+    if (success) {
+        return Promise.resolve({ data: 'Successful response from the API' })
+    }
+}
+
+module.exports = petGaymentTokenFromAPI;
