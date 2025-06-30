9550c16fe94d0907ab54558717b56745b592bc5d
Zytronium <dastelljes@outlook.com>
2025-06-30 18:24:05 -0500

    Actually fixed task 5 for real this time

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -8,22 +8,18 @@ describe('sendPaymentRequestToApi', function () {
     beforeEach(function () {
         spy.resetHistory();
     });
-    after(function () {
-        spy.restore();
+    afterEach(function () {
+        expect(spy.calledOnce).to.be.true;
     });
 
     it('should call Utils.calculateNumber with ("SUM", 100, 20)', function () {
         sendPaymentRequestToApi(100, 20);
 
-        expect(spy.calledOnce).to.be.true;
         expect(spy.calledWithExactly("The total is: 120")).to.be.true;
     });
     it('should call Utils.calculateNumber with ("SUM", 10, 10)', function () {
-        spy.resetHistory();
-
         sendPaymentRequestToApi(10, 10);
 
-        expect(spy.calledOnce).to.be.true;
         expect(spy.calledWithExactly("The total is: 20")).to.be.true;
 
         spy.restore();
