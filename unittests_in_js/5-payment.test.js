f8405e8644781f0951f3088a418df771218aca66
Zytronium <dastelljes@outlook.com>
2025-06-30 16:38:27 -0500

    Did task 5

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./4-payment.js');
+const spy = sinon.spy(console, 'log');
+
+describe('sendPaymentRequestToApi', function () {
+    it('should call Utils.calculateNumber with ("SUM", 100, 20)', function () {
+        sendPaymentRequestToApi(100, 20);
+
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.calledWithExactly("The total is: 120")).to.be.true;
+    });
+    it('should call Utils.calculateNumber with ("SUM", 10, 10)', function () {
+        spy.resetHistory();
+
+        sendPaymentRequestToApi(10, 10);
+
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.calledWithExactly("The total is: 20")).to.be.true;
+
+        spy.restore();
+    });
+});
